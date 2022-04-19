8d65658694c409de51b0175ee194b0cb96bf4fc4
Juanx65 <juan.aguileraca@sansano.usm.cl>
2022-04-19 02:44:36 -0400

    comentar math.h en linux

--- VITIS_HLS_SRC/eucHW.h
+++ VITIS_HLS_SRC/eucHW.h
@@ -2,8 +2,8 @@
 #define EUCDISTHW_H
 
 #include "specs.h"
-#include <cmath>
-#include <math.h>
+#include <cmath> // usar en linux
+//#include <math.h> /* Mantener comentada en linux */
 #include <hls_math.h>  /* Se incluye para el uso de la función hls::sqrt
                         * que disminuye la latencia respecto a sdt::sqrt,
                         * con el costo de tener un mayor error.
--- VITIS_HLS_SRC/eucSW.h
+++ VITIS_HLS_SRC/eucSW.h
@@ -2,7 +2,7 @@
 #define EUCDISTSW_H
 
 #include "specs.h"
-#include <math.h>
+//#include <math.h> /* mantener comentada en linux */
 #include <cmath> // para linux
 
 void eucSW (T A[LENGTH], T B[LENGTH], float C[1]);
